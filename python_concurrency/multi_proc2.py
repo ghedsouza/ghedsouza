6fa9d5d2f4580c920bb396e5c404f609e6d06b3b
Edward D'Souza <edward.dsouza@polarmobile.com>
2020-06-21 19:18:52 -0400

    wip.

--- python_concurrency/multi_proc2.py
+++ python_concurrency/multi_proc2.py
@@ -1,25 +1,29 @@
 import itertools
 import multiprocessing
+import math
 
-from algorithm import closest_pair, distance, generate_points
+from algorithm import closest_pair, generate_points
 
 
 def segment_list(l, N):
     chunk_size, remainder = divmod(len(l), N)
-    first, rest = l[:chunk_size + remainder], l[chunk_size + remainder:]
+    first, rest = l[: chunk_size + remainder], l[chunk_size + remainder :]
     return itertools.chain([first], zip(*[iter(rest)] * chunk_size))
 
 
-points = generate_points()
-
-N = multiprocessing.cpu_count()  # 8 for me.
-segments = segment_list(points, N)
-
-def do_it(segment):
+def do_it(segment, points):
     return closest_pair(segment, points)
 
-pool = multiprocessing.Pool(processes=N)
-results = pool.map(do_it, segments)
 
-closest = min(results, key=lambda res: distance(res[0], res[1]))
-print(closest)
+if __name__ == "__main__":
+    N = multiprocessing.cpu_count()  # 8 for me.
+
+    points = generate_points()
+    segments = list(segment_list(points, N))
+    proc_args = list(zip(segments, [points] * N))
+
+    pool = multiprocessing.Pool(processes=N)
+    results = pool.starmap(do_it, proc_args)
+
+    closest = min(results, key=lambda res: math.dist(res[0], res[1]))
+    print(closest)
--- python_concurrency/multi_thread1.py
+++ python_concurrency/multi_thread1.py
@@ -1,18 +1,19 @@
+import math
 import queue
 import threading
 
-from algorithm import closest_pair, distance, generate_points
+from algorithm import closest_pair, generate_points
 
 points = generate_points()
 
 q = queue.Queue()
 x1 = threading.Thread(
     target=lambda *args: q.put(closest_pair(*args)),
-    args=(points[:len(points) // 2], points),
+    args=(points[: len(points) // 2], points),
 )
 x2 = threading.Thread(
     target=lambda *args: q.put(closest_pair(*args)),
-    args=(points[len(points) // 2:], points),
+    args=(points[len(points) // 2 :], points),
 )
 x1.start()
 x2.start()
@@ -22,5 +23,5 @@ x2.join()
 res1 = q.get()
 res2 = q.get()
 
-closest = min([res1, res2], key=lambda res: distance(res[0], res[1]))
+closest = min([res1, res2], key=lambda res: math.dist(res[0], res[1]))
 print(closest)
